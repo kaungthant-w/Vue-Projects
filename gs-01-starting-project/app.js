bfe796645de71e461c004d465d1acdf29402d331
kaungthant-w <kaungthant9513@gmail.com>
2022-08-26 23:38:56 +0630

    rebuild the app with vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,14 +1,30 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+Vue.createApp({
+    data() {
+        return {
+            goals : [],
+            enteredValue: ""
+        }; 
+    }, 
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    },
+}).mount("#app");
 
-function addGoal() {
-    const enterValue = inputEl.value;
-    const listItemEl = document.createElement("li");
-    listItemEl.textContent = enterValue;
-    listEl.appendChild(listItemEl);
 
-    inputEl.value = "";
-}
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-buttonEl.addEventListener("click", addGoal);
+// function addGoal() {
+//     const enterValue = inputEl.value;
+//     const listItemEl = document.createElement("li");
+//     listItemEl.textContent = enterValue;
+//     listEl.appendChild(listItemEl);
+
+//     inputEl.value = "";
+// }
+
+// buttonEl.addEventListener("click", addGoal);
