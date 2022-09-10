bea25f2c5c9138924fc85a04107367b6169145f0
kaungthant-w <kaungthant9513@gmail.com>
2022-09-10 10:22:06 +0630

    003 Interpolation and Data Binding

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal:"Finish The Course and Learn Vue!"
+            courseGoal:"Finish The Course and Learn Vue!",
+            arrayData:[32, 332, 22, 19, 93],
+            boolData:false,
+            courseNum : 1995
         }
     },
 });
+
+app.mount("#user-goal");
