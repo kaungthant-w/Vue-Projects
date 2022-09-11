cde6caf4bd91c13112e41dbd54c23e100543df03
kaungthant-w <kaungthant9513@gmail.com>
2022-09-11 19:16:43 +0630

    006 Working with Data inside of a Vue App

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal:"Finish The Course and Learn Vue!",
+            courseGoalA:"Finish The Course and Learn Vue!",
+            courseGoalB:" Master Vue and Build amazing App! ",
             arrayData:[32, 332, 22, 19, 93],
             boolData:false,
             courseNum : 1995,
@@ -12,11 +13,18 @@ const app = Vue.createApp({
     methods: {
 
         outputGoal() {
-            const randomNum = Math.random();
+            const randomNum1 = Math.random();
             if( randomNum < 0.5) {
                 return "Learn Vue";
             } else {
                 return "Master Vue";
+            }
+
+            const randomNum2 = Math.random() + 4;
+            if( randomNum2 < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
             }
         }
     },
