39f5be85817996afc5cfe5e1dceb8d5f3087d154
kaungthant-w <kaungthant9513@gmail.com>
2022-09-11 19:07:41 +0630

    005 Understanding methods in Vue Apps

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -4,7 +4,20 @@ const app = Vue.createApp({
             courseGoal:"Finish The Course and Learn Vue!",
             arrayData:[32, 332, 22, 19, 93],
             boolData:false,
-            courseNum : 1995
+            courseNum : 1995,
+            vueLink : "https://www.aprogrammer.blog/learn/laravel"
+        }
+    },
+
+    methods: {
+
+        outputGoal() {
+            const randomNum = Math.random();
+            if( randomNum < 0.5) {
+                return "Learn Vue";
+            } else {
+                return "Master Vue";
+            }
         }
     },
 });
